5e2ba7c6c1ef8566cf44448a2e8a12ec60561930
Esther Veschambre <esther.veschambre@cpe.fr>
2022-12-15 10:19:53 +0100

    window v1

--- window.py
+++ window.py
@@ -7,15 +7,35 @@ Fenetre de jeu
 """
 
 
-from tkinter import Tk, Label, Button, PhotoImage
+from tkinter import Tk, Frame, Label, Button, PhotoImage, Canvas
 
 window = Tk()
 window.title("Space Invaders")
 
-labelScore = Label(window, text = "Score = ")
+#Création canevas
+height = 400
+width = 300
+photo_background = PhotoImage(file = "background.png")
+canevas = Canvas(window, width = width, height = height, bg = 'black')
+canevas.pack()
+
+#Création des différentes frames
+labelScore = Label(window, text = "Score = ", relief = "flat") #definir score
 labelScore.pack()
+
+Menu = Frame(window)
+
+
+
+"""
+buttonStart = Button(window, text = "Start game", command = Start) #fefinir fonction start
+buttonStart.pack()
+"""
 
 buttonQuitter = Button(window, text = "Quit game", command = window.destroy)
 buttonQuitter.pack()
+
+
+
 
 window.mainloop()
