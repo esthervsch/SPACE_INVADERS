2d7cb5af9f7241e4f9b15613561eeca872382c94
Beber <bertra.gaillet@gmail.com>
2022-12-15 09:29:36 +0100

    redecouverte de git

---
+++ Main_bertrand.py
@@ -0,0 +1,9 @@
+class players :
+    def __init__(self, Hitbox, Hp, Score, Name, Life, Position):
+        self.Hitbox = [1,1]
+        self.Life = 3
+        self.Score = "0"
+        self.Name = "no name"
+        self.Hp = 1
+        self.Position = [0,1]
+
