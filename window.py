9df5482f934519b9de5993a540a8ffa034625bae
Esther Veschambre <esther.veschambre@cpe.fr>
2022-12-15 15:30:09 +0100

    backgroung ca ma tendu fair  sur ordi

--- window.py
+++ window.py
@@ -6,20 +6,25 @@ Space_Invaders
 Fenetre de jeu
 """
 
-
-from tkinter import *
-
+from tkinter import * #morrel dit ok
 
 window = Tk()
 window.title("Space Invaders")
 
 #Création canevas
-height = 800
+height = 800 
 width = 600
+
+canvas = Canvas(window, width = width, height = height)
+canvas.pack()
+
+#mise en place background
 photo_background = PhotoImage(file = "background.png")
-canevas = Canvas(window, width = width, height = height, bg = 'black')
-canevas.pack()
-Score =0
+photo_backgroundbis = photo_background.zoom(x = )
+canvas.create_image(0, 0, image=photo_backgroundbis)
+
+
+
 #Création des différentes frames
 
 
@@ -37,23 +42,25 @@ buttonQuitter.pack()
 
 
 #Score à récup via objet player in game
-Score = 0
+score="a recup"
 #Add a text in Canvas
-canevas.create_text(150, 50, text="Score : "+str(Score), fill="white", font=('Helvetica 15 bold'))
-canevas.pack()
+canvas.create_text(150, 50, text="Score : "+str(score), fill="white", font=('Helvetica 15 bold'))
+canvas.pack()
+
 #déplacement alien
-img_alien = PhotoImage(file="Alien.png")
-Alien = canevas.create_image(10, 10, anchor= N, image=img_alien)
+"""
+img_alien = PhotoImage(file="background.png")
+Alien = canvas.create_image(10, 10, anchor= N, image=img_alien)
 x1 = 20 
 y1 = 20
-missile = canevas.create_rectangle(x1, y1, x1 + 10, y1 + 50,fill='red')
+missile = canvas.create_rectangle(x1, y1, x1 + 10, y1 + 50,fill='red')
 
 def move(event):
     if event.char == "q":
-        canevas.move(Alien, -10, 0)
+        canvas.move(Alien, -10, 0)
     elif event.char == "d":
-        canevas.move(Alien, 10, 0)
+        canvas.move(Alien, 10, 0)
 window.bind("<Key>", move)
-
+"""
 
 window.mainloop()
