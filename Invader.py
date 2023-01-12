eab1908ba352177a6c5ad833cae818d205146c3e
Esther Veschambre <esther.vsch@gmail.com>
2023-01-12 18:54:24 +0100

    invaders pop up et mouvement sans bordure

--- Invader.py
+++ Invader.py
@@ -6,13 +6,13 @@ Space_Invaders
 Classe Invader
 """
 from Game import Game
-from time import sleep
 game = Game(1) #a retier
 class Invader :                #cette classe décrit 3 types d'invaders
-    def __init__(self, coordX, coordY, type) :
+    def __init__(self, coordX, coordY, type, name) :
         self.coordX = coordX
         self.coordY = coordY
         self.type = type
+        self.name = name
         self.Hp = [100*game.difficulty, 150*game.difficulty, 250*game.difficulty]
         self.speed = game.speed         #dépend du level
         self.freq = 3
@@ -26,12 +26,12 @@ class Invader :                #cette classe décrit 3 types d'invaders
         #type 2 :mouvements tranversaux
         #type 3 : descend et mouvements transversaux
         if self.type == 1 :
-            self.coordY += self.speed       #on ajout à l'ordonnée une valeur dépendant du niveau 
+            self.coordY += self.speed*5       #on ajout à l'ordonnée une valeur dépendant du niveau 
         if self.type == 2 :
-            self.coordX += self.speed
+            self.coordX += self.speed*5
         if self.type == 3 :
-            self.coordX += self.speed
-            self.coordY += self.speed
+            self.coordX += self.speed*5
+            self.coordY += self.speed*5
 
     #def fire(self) :
         
--- estherpourwondow.py
+++ estherpourwondow.py
@@ -34,34 +34,38 @@ player_view = canvas.create_image(player.coordX, player.coordY, image=img_player
 
 #images des invaders
 list_invader = [] #liste des invaders en tant qu'objet
-list_invader_view = [] #liste des invaders en tant qu'objets du canvas
+list_invader_img = [] #liste des invaders en tant qu'images
+for i in range(100) :
+    globals()['img_invader' + str(i-1)] = None
+    list_invader_img.append('img_invader' + str(i-1))
 
-
-
+compte = 0
 def pop_up_invader() :          #gère l'apparition des invaders
-    
     coordX = width/2
     coordY = 100
     types = random.choice([1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,3]) #genère un nombre aléatoire entre 1 et 3 avec différentes probabilités pour choisir le type d'invader :(1, 2, 3), p=[0.5, 0.35, 0.15]
-    invader = Invader(coordX, coordY, types)  #centre de la fenetre en haut
+    global compte
+    name = 'invader' + str(compte) 
+    print(name)
+    invader = Invader(coordX, coordY, types, name)  #centre de la fenetre en haut
     invader.imagefile = invader.imagefile[invader.type-1] #on lui assigne l'image corespondante (remplace la liste par le choix)
     invader.Hp = invader.Hp[types-1]
     list_invader.append(invader) #On ajoute l'invader à la liste d'invaders
+    global list_invader_img
+    list_invader_img[compte]= PhotoImage(file = invader.imagefile)
+    invader.name = canvas.create_image(invader.coordX, invader.coordY, image=list_invader_img[compte])
+    compte += 1
     window.after(5000, pop_up_invader) #nouvel invader toutes les 10s
     return list_invader
-pop_up_invader()#on attend pas pour faire apparaitre le premier invader
+pop_up_invader() #on attend pas pour faire apparaitre le premier invader
 
-newinvader = list_invader[len(list_invader)-1]
-img_invader = PhotoImage(file = newinvader.imagefile)
-invader_view = canvas.create_image(newinvader.coordX, newinvader.coordY, image=img_invader)
-list_invader_view.append(invader_view) #On ajoute le canvas de l'invader à la liste des canvas d'invaders
 
 
 #Déplacement des invaders
 def move_invaders() :
-    for i, val in enumerate(list_invader):
+    for val in list_invader :
         val.move()
-        canvas.moveto(list_invader_view[i-1], val.coordX , val.coordY)
+        canvas. coords(val.name, val.coordX , val.coordY)
     window.after(1000, move_invaders) #répete le mouvement toutes les secondes
 move_invaders()
 
@@ -79,8 +83,5 @@ def clavier(event) :
     canvas.coords(player_view, player.coordX , player.coordY)
 window.bind('<Key>',clavier)
 
-
+        
 window.mainloop()
-
-
-
